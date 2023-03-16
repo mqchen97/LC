061f1303fce52956bfbece430d4b2dc8cecf7649
zgjswxcmq <939082952@qq.com>
2023-03-16 08:56:07 +0800

    update 75 注释

--- 75_颜色分类.js
+++ 75_颜色分类.js
@@ -10,19 +10,20 @@ var sortColors = function (nums) {
         if (nums[i] === 0) {
             p = p + 1
             swap(i, p)
-            //因为扫描过的并且还在前面的肯定是1
+            //当i===p，说明边界后一位还是0，其实swap直接返回; 如果i > p,p后一位肯定是1，此时遇到0，只需要将0 1位置换一下
             i++
         } else if (nums[i] === 2) {
             q = q - 1
+            //将2移动到后面，i不变，因为替换的元素没有扫描过，是0 1 2都有可能，需要对它再做判断
             swap(i, q)
-            //后面的没扫描过，所以得再看一下
         } else {
-            //直接跳过，根据==0时替换
+            //遇到1不需要将它放进边界内，直接跳过
             i++
         }
     }
 
     function swap(i, j) {
+        if (i === j) return;
         let temp = nums[i]
         nums[i] = nums[j]
         nums[j] = temp
